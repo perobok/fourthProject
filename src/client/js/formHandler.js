a367de6830febaf6693c1fad34c812e03d98c40a
perobok <bokicp@yahoo.com>
2021-02-21 20:25:23 +0100

    secondCommit

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,16 +1,42 @@
-function handleSubmit(event) {
-    event.preventDefault()
+const apiKey = '8e48286ca5ff3760915b968299ee8903'
 
-    // check what text was put into the form field
+function handleSubmit(e) {
+    e.preventDefault()
+    console.log(process.env.API_KEY)
+        // check what text was put into the form field
     let formText = document.getElementById('name').value
     Client.checkForName(formText)
 
+    const APIcall = getData(formText);
+
     console.log("::: Form Submitted :::")
-    fetch('http://localhost:8081/test')
-        .then(res => res.json())
-        .then(function(res) {
-            document.getElementById('results').innerHTML = res.message
-        })
+    async function getData(formText) {
+        const response = await fetch(`https://api.meaningcloud.com/sentiment-2.1?key=${apiKey}&of=json&txt=${formText}&lang=en`);
+        const data = await response.json();
+        console.log(data);
+        const update = updateUI(data)
+        return data;
+
+    }
+
+    const updateUI = async(data) => {
+            document.getElementById('results1').innerHTML = 'Subjectivity: ' + data.subjectivity;
+            document.getElementById('results2').innerHTML = 'Confidence: ' + data.confidence + '%';
+            document.getElementById('results3').innerHTML = 'Irony: ' + data.irony;
+        }
+        /*
+            fetch(`https://api.meaningcloud.com/sentiment-2.1?key=<API_KEY>&of=json&url=${formText}&lang=en`, {
+                    method: "POST",
+                    credentials: 'same-origin',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify({}),
+                })
+                .then(res => res.json())
+
+            .then(function(res) {
+                document.getElementById('results').innerHTML = res.message
+            })
+             */
 }
 
 export { handleSubmit }
--- src/server/index.js
+++ src/server/index.js
@@ -1,7 +1,11 @@
+const dotenv = require('dotenv');
+dotenv.config();
 var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
 
+
+console.log(`Your API key is ${process.env.API_KEY}`);
 const app = express()
 
 app.use(express.static('dist'))
