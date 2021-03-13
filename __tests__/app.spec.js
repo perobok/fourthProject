ca76369a6943c153491e7c17bac00e97189a3fdd
perobok <bokicp@yahoo.com>
2021-03-13 23:08:14 +0100

    ReadMe updated

--- __tests__/app.spec.js
+++ __tests__/app.spec.js
@@ -1,4 +1,4 @@
-import { geoData } from '../src/client/js/testing';
+import { geoData } from '../src/client/js/app';
 
 test('check if entered date is higher than today', () => {
     expect(geoData('2021-3-2', '2021-2-25')).toBeGreaterThanOrEqual(Date());
--- __tests__/server.spec.js
+++ __tests__/server.spec.js
@@ -1,4 +1,4 @@
-import { keys } from '../src/server/testing';
+import { keys } from '../src/server/index';
 
 test('checking if the username is included', () => {
     expect(keys('API_USERNAME_GEODATA')).not.toBe('');
